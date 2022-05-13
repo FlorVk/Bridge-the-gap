a9d17157c46183335f8cc14cfd309eeeda7f6d22
FlorVk <florvankuyk@hotmail.com>
2022-05-13 19:09:03 +0200

    fix: added cors

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server,)
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
 const { v4: uuidV4 } = require('uuid')
+
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -18,10 +25,10 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.to(roomId).emit('user-connected', userId)
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      socket.to(roomId).emit('user-disconnected', userId);
     })
   })
 })
